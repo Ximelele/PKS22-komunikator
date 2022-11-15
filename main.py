035967e902ecafa1dd7cd0d66e57b62a548a8158
Ximelele <druzbacky.martin@gmail.com>
2022-11-15 22:16:36 +0100

    Pridanie fragmentovania. Prerobenie posielanie sprav na fragmentovanie

--- main.py
+++ main.py
@@ -42,7 +42,8 @@ def crc16(data: bytes):
     return reg ^ xor_out
 
 
-def build_header(flag, packet_number, data,file=False):
+def build_header(flag, packet_number, data, file=False):
+
     if file:
         crc = crc16(data)
     else:
@@ -67,6 +68,19 @@ def build_header(flag, packet_number, data,file=False):
 server = None
 SYN = False
 
+MAX_DATA_SIZE = 1468
+
+
+def choose_fragment_size():
+    print("Vyber velkost fragmentu 1-1468")
+    size = int(input())
+    global MAX_DATA_SIZE
+    MAX_DATA_SIZE = 1468
+    if size < 0:
+        MAX_DATA_SIZE = 1
+    elif size <= MAX_DATA_SIZE:
+        MAX_DATA_SIZE = size
+
 
 def server_loop():
     global server
@@ -76,7 +90,7 @@ def server_loop():
     global SYN
     file_name = ""
     while True:
-        message, message_add = server.server_socket.recvfrom(2048)
+        message, message_add = server.server_socket.recvfrom(1500)
         if not SYN:
             my_header = build_header(2, 0, "")
             server.server_socket.sendto(my_header, message_add)
@@ -91,9 +105,8 @@ def server_loop():
         finalMsg = message[2:]
 
         if flag == 5:
-            print(f'flag {flag} cislo {packet_num} message {finalMsg.decode()}')
-            my_header = build_header(2, 0, "")
-            server.server_socket.sendto(my_header, message_add)
+            receive_text(finalMsg, server,message_add)
+
 
         if flag == 6:
             file_name = finalMsg.decode().strip()
@@ -101,6 +114,38 @@ def server_loop():
             server.server_socket.sendto(my_header, message_add)
             receive_file(file_name, flag, server)
 
+
+def receive_text(textMsg,server,message_add):
+    textArray = []
+    textArray.append(textMsg.decode())
+    my_header = build_header(4, 0, "")
+    server.server_socket.sendto(my_header, message_add)
+    number_of_fragments = 1
+    total_size = len(textMsg)
+    while True:
+        message, message_add = server.server_socket.recvfrom(1500)
+
+        crc = message[-2:]
+        crc = int.from_bytes(crc, 'little')
+        message = message[0:-2]
+
+        flag = int(chr(message[0]))
+        if flag == 9:
+            break
+
+        packet_num = int(chr(message[1]))
+        total_size += len(message[2:])
+        textArray.append(message[2:].decode())
+        my_header = build_header(4, 0, "")
+        server.server_socket.sendto(my_header, message_add)
+        number_of_fragments +=1
+
+    my_header = build_header(9, 0, "")
+    server.server_socket.sendto(my_header, message_add)
+    final_text = "".join(textArray)
+    print(final_text)
+    print(f'Pocet prijatych fragmentov {number_of_fragments}')
+    print(f'Celkova prijata velkost fragmentov {total_size}')
 
 
 def receive_file(file_name, flag, server):
@@ -123,22 +168,19 @@ def receive_file(file_name, flag, server):
         #     print("Pici tam")
         #     break
 
-        file_array[str(packet_num)]=partial_file
+        file_array[str(packet_num)] = partial_file
 
-    #file_array=dict(sorted(file_array.keys(), key=lambda item: item[1])
-    fw = open("pici.pdf",'wb')
+    # file_array=dict(sorted(file_array.keys(), key=lambda item: item[1])
+    fw = open("pici.pdf", 'wb')
 
     for i in file_array.values():
         fw.write(i)
 
 
-
-
-
 client = None
 
-def client_menu():
 
+def client_menu():
     menu = "t - textova sprava\n"
     menu += "f - poslat subor\n"
     menu += "s - zmenit strany\n"
@@ -147,21 +189,53 @@ def client_menu():
 
     return str(input())
 
-def send_file(file,client):
 
-    f = open(file,'rb')
-    fragment_size = 10
-    data = f.read(2048)
+# todo prerobit posielanie suboru
+def send_file(file, client):
+    f = open(file, 'rb')
+    data = f.read()
+
+    print(len(data))
+
     fragment = 0
 
     while data:
-        my_header = build_header(6, fragment, data,True)
+        my_header = build_header(6, fragment, data, True)
         client.client_socket.sendto(my_header, client.serverAddressPort)
-        fragment +=1
+        fragment += 1
         data = f.read(2048)
 
-    my_header = build_header(9, fragment+1, "")
+    my_header = build_header(9, fragment + 1, "")
     client.client_socket.sendto(my_header, client.serverAddressPort)
+
+
+def send_text(textMsg, client):
+    textArray = []
+    fragments_to_send = 0
+    text_len = len(textMsg)
+    global MAX_DATA_SIZE
+    if text_len > MAX_DATA_SIZE:
+        while True:
+                if len(textMsg) > MAX_DATA_SIZE:
+                    textArray.append(textMsg[:MAX_DATA_SIZE])
+                    textMsg = textMsg[MAX_DATA_SIZE:]
+                else:
+                    textArray.append(textMsg[:len(textMsg)])
+                    break
+    else:
+        textArray = textMsg
+
+    for index, value in enumerate(textArray,start=1):
+        my_header = build_header(5, index%9, value)
+        client.client_socket.sendto(my_header, client.serverAddressPort)
+        message, message_add = client.client_socket.recvfrom(1500)
+        fragments_to_send = index
+
+    my_header = build_header(9, fragments_to_send+1, "")
+    client.client_socket.sendto(my_header, client.serverAddressPort)
+    message, message_add = client.client_socket.recvfrom(1500)
+
+
 
 def client_loop():
     global client
@@ -172,17 +246,18 @@ def client_loop():
         if not SYN:
             my_header = build_header(1, 0, "")
             client.client_socket.sendto(my_header, client.serverAddressPort)
-            message, message_add = client.client_socket.recvfrom(2048)
-            SYN=True
+            message, message_add = client.client_socket.recvfrom(1500)
+            SYN = True
             continue
         choice = client_menu()
 
         if choice == "t":
             print("Zadaj spravu")
             textMsg = str(input())
-            my_header = build_header(5, 0, textMsg)
-            client.client_socket.sendto(my_header, client.serverAddressPort)
-            message, message_add = client.client_socket.recvfrom(2048)
+            choose_fragment_size()
+            send_text(textMsg, client)
+
+
         elif choice == "f":
             print("Zadaj subor")
             file = str("utrpenie.pdf")
@@ -190,8 +265,9 @@ def client_loop():
             print(file_name)
             my_header = build_header(6, 0, file_name)
             client.client_socket.sendto(my_header, client.serverAddressPort)
-            message, message_add = client.client_socket.recvfrom(2048)
-            send_file(file,client)
+            message, message_add = client.client_socket.recvfrom(1500)
+            send_file(file, client)
+
 
 opt = str(input())
 
